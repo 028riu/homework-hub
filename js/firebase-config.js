7609e6ac0d50efed94f3952951fd8d1126918550
028riu <028riu@gmail.com>
2026-08-31 10:29:01 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,17 @@
-// Public Firebase web configuration. Firebase API keys are not passwords.
-export const firebaseConfig = {
-  apiKey: "AIzaSyAahQiF9OONWrQIbs-lfQKUAzkG9fov-IcQ",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAahQiF9OONWrQIbs-lfQKUAzkG9fov-Ic",
   authDomain: "homeworkhuba5.firebaseapp.com",
   projectId: "homeworkhuba5",
   storageBucket: "homeworkhuba5.firebasestorage.app",
   messagingSenderId: "226835406713",
   appId: "1:226835406713:web:157c0f890ed7385b2319a8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
