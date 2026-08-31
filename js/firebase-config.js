6a97815b8bfe4ec7b21143c689dfb91332ee28e6
028riu <028riu@gmail.com>
2026-08-31 10:34:08 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Homework Hub Firebase configuration
+// Project: homeworkhuba5
+export const firebaseConfig = {
   apiKey: "AIzaSyAahQiF9OONWrQIbs-lfQKUAzkG9fov-Ic",
   authDomain: "homeworkhuba5.firebaseapp.com",
   projectId: "homeworkhuba5",
@@ -12,6 +8,3 @@ const firebaseConfig = {
   messagingSenderId: "226835406713",
   appId: "1:226835406713:web:157c0f890ed7385b2319a8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
