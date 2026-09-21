0a7ac1c5edd997ce2d149e50376bbc817281af0e
028riu <028riu@gmail.com>
2026-09-21 23:25:46 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='homework-hub-pwa-v3';
+const CORE=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  const r=e.request;
+  if(r.method!=='GET') return;
+  if(new URL(r.url).origin!==location.origin) return;
+  e.respondWith(fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res}).catch(()=>caches.match(r).then(x=>x||caches.match('./index.html'))));
+});
